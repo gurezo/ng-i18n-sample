28a3f9ac1c846b631088719af4ff01ae83d948c6
gurezo <akihiko.kigure@gmail.com>
2019-12-11 23:00:53 +0900

    home, about, not-fouund add.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NavigatorComponent  } from './navigator/navigator.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'en', pathMatch: 'full' },
@@ -8,31 +11,31 @@ const routes: Routes = [
     path: ':lang', component: NavigatorComponent,
     // resolve: { content: ContentResolver },
     // data: { source: "navigator.json" },
-    // children: [
-    //   { path: '', redirectTo: 'home', pathMatch: 'full' },
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-    //   {
-    //     path: 'home',
-    //     component: HomeComponent,
-    //     resolve: { content: ContentResolver },
-    //     data: { source: "home.json" }
-    //   },
+      {
+        path: 'home',
+        component: HomeComponent,
+        // resolve: { content: ContentResolver },
+        // data: { source: "home.json" }
+      },
 
-    //   {
-    //     path: 'about',
-    //     component: AboutComponent,
-    //     resolve: { content: ContentResolver },
-    //     data: { source: "about.json" }
-    //   },
+      {
+        path: 'about',
+        component: AboutComponent,
+        // resolve: { content: ContentResolver },
+        // data: { source: "about.json" }
+      },
 
-    //   {
-    //     path: 'not-found',
-    //     component: NotFoundComponent,
-    //     resolve: { content: ContentResolver },
-    //     data: { source: "not-found.json" }
-    //   },
-    //   { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
-    // ]
+      {
+        path: 'not-found',
+        component: NotFoundComponent,
+        // resolve: { content: ContentResolver },
+        // data: { source: "not-found.json" }
+      },
+      { path: '**', redirectTo: 'not-found', pathMatch: 'full' }
+    ]
   }
 ];
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigatorComponent } from './navigator/navigator.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, NavigatorComponent],
+  declarations: [AppComponent, NavigatorComponent, HomeComponent, AboutComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
