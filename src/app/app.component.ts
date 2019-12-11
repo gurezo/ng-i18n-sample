325b0e91365a850e3849052acdfd9b96124af90c
gurezo <akihiko.kigure@gmail.com>
2019-12-11 22:22:57 +0900

    excute error resolve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,23 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subject, Subscription, timer } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { switchMap, delay, takeUntil, tap } from 'rxjs/operators';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
   title = 'ng-i18n-sample';
 
   private destroy = new Subject<void>();
   private sub: Subscription;
 
   constructor(
-    private translate: TranslateService,
     private http: HttpClient
   ) {
-    // this language will be used as a fallback when a translation isn't found in the current language
-    // this.translate.setDefaultLang('en');
-    this.translate.setDefaultLang('ja');
-
-    // the lang to use, if the lang isn't available, it will use the current loader to get them
-    // this.translate.use('en');
-    this.translate.use('ja');
   }
 
   ngOnInit(): void {
-    // this.sub = timer(2000)
-    //   .pipe(
-    //     switchMap(() => this.http.get('/assets/test1.txt')),
-    //     tap(() => console.log()),
-    //     delay(3000),
-    //     takeUntil(this.destroy)
-    //   )
-    //   .subscribe(res => {
-    //     console.log(res);
-    //   });
-  }
-
-  ngOnDestroy() {
-    console.log(`before ${this.sub.closed}`);
-    this.destroy.complete();
-    console.log(`after ${this.sub.closed}`);
-  }
-
-  login() {
-
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,12 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 // import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    TranslateModule.forRoot({
-      loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
-      }
-    }),
     // AppRoutingModule
   ],
   providers: [],
@@ -26,8 +17,3 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 })
 export class AppModule {}
 
-
-// required for AOT compilation
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
-}
